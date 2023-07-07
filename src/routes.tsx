dce0abeae463cd051bc356b65614a07c47ca6fa8
Betsy Babu John <betsyjohn03082021@gmail.com>
2023-07-07 16:14:41 -0500

    coffee-design the login page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,26 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Login from './modules/login/login';
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./modules/login/pages/login";
 
 const RoutesList: React.FC = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
-
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/quotes" element={<Quotes />} />
-        <Route path="/questions" element={<Questions />} />
-        <Route path="/webinars" element={<Webinars />} /> */}
       </Routes>
     </>
   );
 };
 
 export default RoutesList;
-
